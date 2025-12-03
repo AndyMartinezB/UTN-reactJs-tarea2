7f17c53ab67e4d27a2b190a8ebf9831fe964e3b7
Andy <andres.bologna@mi.unc.edu.ar>
2025-12-03 19:59:53 -0300

    init: creacion de repo

---
+++ README.md
@@ -0,0 +1,12 @@
+Descripción breve del proyecto.
+
+○ Instrucciones para clonar, instalar dependencias (si aplica) y ejecutar en
+un navegador.
+
+○ Capturas de pantalla mostrando el formulario en funcionamiento
+(mensaje positivo y negativo).
+
+○ Créditos del autor (nombre del estudiante, curso, unidad).
+
+○ Citación de fuentes en el README.md (bibliografía y créditos de imágenes
+si corresponde).
---
+++ form/javascript-tarea-2.js
@@ -0,0 +1,22 @@
+// usar document.querySelector.
+
+const form = document.querySelector("#form_suscripcion");
+const nombreInput = document.querySelector("#nombre_usuario");
+const edadInput = document.querySelector("#edad_usuario");
+const parrafoRespuesta = document.querySelector("#respuesta");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const nombreUsuario = nombreInput.value;
+  const numeroUsuario = edadInput.value;
+
+  if (numeroUsuario >= 18) {
+    parrafoRespuesta.textContent = `✅ Bienvenido, ${nombreUsuario}, tienes acceso al evento.`;
+    parrafoRespuesta.classList.add("borde-verde");
+    parrafoRespuesta.classList.remove("borde-rojo");
+  } else {
+    parrafoRespuesta.textContent = `❌ Lo sentimos ${nombreUsuario}, debes ser mayor de edad.`;
+    parrafoRespuesta.classList.add("borde-rojo");
+    parrafoRespuesta.classList.remove("borde-verde");
+  }
+});
